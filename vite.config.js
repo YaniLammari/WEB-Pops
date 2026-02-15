d455c10f3d9259b8dbb07147228829531f2efbf5
Yani Lammari <yani.lammari@lycee-jeanrostand.fr>
2026-02-15 21:53:31 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/WEB-Pops/', // Ajoutez cette ligne avec le nom exact de votre dépôt
-})
